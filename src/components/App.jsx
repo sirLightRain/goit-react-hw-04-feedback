504cfd498fe14c8b52859f6fea5dfaccce8d4cc5
Oleksii Stepchuk <34480855+sirLightRain@users.noreply.github.com>
2023-09-12 14:23:23 +0300

    App рефакторинг на хуки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,67 +1,65 @@
 import { GlobalStyle } from 'GlobalStyle';
 import { Layout } from 'Layuot';
-import { Component } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from './Buttons/Buttons';
 import { NoFeetback } from './NoFeetback/NoFeetback';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleFeedback = feedbackType => {
+    if (feedbackType === 'good') {
+      setGood(good + 1);
+    } else if (feedbackType === 'neutral') {
+      setNeutral(neutral + 1);
+    } else if (feedbackType === 'bad') {
+      setBad(bad + 1);
+    }
   };
 
-  handleFeedback = feedbackType => {
-    this.setState(prevState => ({
-      [feedbackType]: prevState[feedbackType] + 1,
-    }));
-  };
-
-  calculateTotal = () => {
-    const { good, neutral, bad } = this.state;
+  const calculateTotal = () => {
     return good + neutral + bad;
   };
 
-  calculatePositivePercentage = () => {
-    const total = this.calculateTotal();
-    return total > 0 ? ((this.state.good / total) * 100).toFixed(2) : 0;
+  const calculatePositivePercentage = () => {
+    const total = calculateTotal();
+    return total > 0 ? ((good / total) * 100).toFixed(2) : 0;
   };
 
-  calculateNoFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const calculateNoFeedback = () => {
     return good === 0 && neutral === 0 && bad === 0;
   };
 
-  render() {
-    const total = this.calculateTotal();
-    const positivePercentage = this.calculatePositivePercentage();
-    const noFeedback = this.calculateNoFeedback();
+  const total = calculateTotal();
+  const positivePercentage = calculatePositivePercentage();
+  const noFeedback = calculateNoFeedback();
 
-    return (
-      <Layout>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.handleFeedback}
+  return (
+    <Layout>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {noFeedback ? (
+          <NoFeetback title="No ffetback given!" />
+        ) : (
+          <Statistics
+            options={{ good, neutral, bad }}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
+        )}
+      </Section>
 
-        <Section title="Statistics">
-          {noFeedback ? (
-            <NoFeetback title="No ffetback given!" />
-          ) : (
-            <Statistics
-              options={this.state}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          )}
-        </Section>
-
-        <GlobalStyle />
-      </Layout>
-    );
-  }
-}
+      <GlobalStyle />
+    </Layout>
+  );
+};
